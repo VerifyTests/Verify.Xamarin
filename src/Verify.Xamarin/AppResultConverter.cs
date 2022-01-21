51a76a7902ce736a735b5dca77b2051b514db1f0
Simon Cropp <simon.cropp@gmail.com>
2022-01-21 20:29:29 +1100

    refs

--- src/Verify.Xamarin/AppResultConverter.cs
+++ src/Verify.Xamarin/AppResultConverter.cs
@@ -1,14 +1,13 @@
-﻿using Newtonsoft.Json;
-using Xamarin.UITest.Queries;
+﻿using Xamarin.UITest.Queries;
 
 class AppResultConverter :
     WriteOnlyJsonConverter<AppResult>
 {
-    public override void Write(VerifyJsonWriter writer, AppResult result, JsonSerializer serializer)
+    public override void Write(VerifyJsonWriter writer, AppResult result)
     {
         var rect = result.Rect;
         var rectString = $"w:{rect.Width} h:{rect.Height} x:{rect.X} y:{rect.Y}";
         var wrapper = new AppResultWrapper(result.Id, result.Label, result.Text, result.Class.Split('.').Last(), rectString);
-        serializer.Serialize(writer, wrapper);
+        writer.Serialize(wrapper);
     }
 }
